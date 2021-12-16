6e260b4e52be942d72d7ecc4ec1d18baf08f46a8
drevnii <ssamozhen13@gmail.com>
2021-12-16 13:22:55 +0400

    Tests

--- parser/src/index.ts
+++ parser/src/index.ts
@@ -3,3 +3,20 @@ function hello() {
 }
 
 hello()
+
+type Visitor = {
+  frequency: number
+  recencyRank?: number
+  frequencyRank?: number
+  monetaryRank?: number
+}
+
+function addFrequency(obj: Visitor): Visitor {
+  return { ...obj, frequencyRank: 0 }
+}
+
+console.log(addFrequency({ frequency: 13 }).frequencyRank === 5)
+console.log(addFrequency({ frequency: 7 }).frequencyRank === 4)
+console.log(addFrequency({ frequency: 4 }).frequencyRank === 3)
+console.log(addFrequency({ frequency: 2 }).frequencyRank === 2)
+console.log(addFrequency({ frequency: 1 }).frequencyRank === 1)
